8f567361cf64a848157cf8acd3615f051c2de588
aman-gigawave <aman@gigawave.io>
2026-04-10 17:30:07 +0530

    Add blog: download-flutter-windows-11 [page]

---
+++ app/blogs/download-flutter-windows-11/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+import GeneratedBlogPage from "./components/GeneratedBlogPage";
+
+export const runtime = "edge";
+
+export const metadata: Metadata = {
+  title: "how to download flutter in windows 11",
+  description: "how to download flutter in windows 11",
+};
+
+const Page = () => {
+  return <GeneratedBlogPage />;
+};
+
+export default Page;
